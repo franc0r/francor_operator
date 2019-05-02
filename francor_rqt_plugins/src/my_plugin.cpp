5dc4f2a9e1777e41e5e732324518599756a912b6
Christian Merkl <knueppl@gmx.de>
2019-05-02 09:41:57 +0200

    Initial commit of francor_rqt_plugins - yeahhhhhh.

---
+++ francor_rqt_plugins/src/my_plugin.cpp
@@ -0,0 +1,44 @@
+/**
+ * THIS IS A FUCKTING HACK AND ONLY FOR ROBOCUP USE!
+ * 
+ * \author Christian Merkl (knueppl@gmx.de)
+ * \date 2. May 2019
+ */
+#include "my_plugin.h"
+
+#include <ros/ros.h>
+// #include <pluginlib/class_list_macros.h>
+// #include <QStringList>
+#include <QApplication>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QProcess>
+
+namespace my_namespace
+{
+
+MyPlugin::MyPlugin()
+  : widget_(0)
+{
+  widget_ = new QWidget();
+
+  auto layout = new QGridLayout;
+  auto stopButton = new QPushButton(QIcon("/home/knueppl/ros/francor/src/francor_operator/francor_rqt_plugins/icon/icon-stop.png"), "");
+  stopButton->setMinimumSize(256, 256);
+  layout->addWidget(stopButton);
+
+  widget_->setLayout(layout);
+  widget_->show();
+}
+
+} // end namespace 
+
+int main(int argc, char** argv)
+{
+  QApplication app(argc, argv);
+  ros::init(argc, argv, "gui_for_schmiddey");
+
+  my_namespace::MyPlugin plugin;
+
+  return app.exec();
+}
---
+++ francor_rqt_plugins/src/my_plugin.h
@@ -0,0 +1,26 @@
+#ifndef my_namespace__my_plugin_H
+#define my_namespace__my_plugin_H
+ 
+// #include <rqt_gui_cpp/plugin.h>
+// #include <ui_my_plugin.h>
+#include <QWidget>
+ 
+namespace my_namespace
+{ 
+
+class MyPlugin : public QObject
+{ 
+  Q_OBJECT
+
+public: 
+  MyPlugin(void);
+  virtual ~MyPlugin(void) = default;
+
+private: 
+  // Ui::MyPluginWidget ui_; 
+  QWidget* widget_; 
+};
+
+} // namespace
+
+#endif // my_namespace__my_plugin_H
