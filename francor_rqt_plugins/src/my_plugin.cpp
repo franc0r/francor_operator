a927555f9c6699aa96b775e5320950b08a26231f
Christian Merkl <knueppl@gmx.de>
2019-05-02 11:12:02 +0200

    Added co2 sensor bar to the widget.

--- francor_rqt_plugins/src/my_plugin.cpp
+++ francor_rqt_plugins/src/my_plugin.cpp
@@ -6,19 +6,22 @@
  */
 #include "my_plugin.h"
 
-#include <ros/ros.h>
 // #include <pluginlib/class_list_macros.h>
 // #include <QStringList>
 #include <QApplication>
 #include <QPushButton>
 #include <QGridLayout>
 #include <QProcess>
+#include <QProgressBar>
+#include <QTimer>
 
 namespace my_namespace
 {
 
 MyPlugin::MyPlugin()
-  : widget_(0)
+  : widget_(0),
+    _priv_nh("~"),
+    _nh()
 {
   widget_ = new QWidget();
 
@@ -28,22 +31,53 @@ MyPlugin::MyPlugin()
   layout->addWidget(stopButton);
   this->connect(stopButton, SIGNAL(pressed()), this, SLOT(stop()));
 
+  _co2bar = new QProgressBar;
+  _co2bar->setMaximum(1024);
+  _co2bar->setMinimum(0);
+  layout->addWidget(_co2bar);
+
   widget_->setLayout(layout);
   widget_->show();
+
+#ifndef Q_MOC_RUN
+  _sub_co2 = _nh.subscribe("/sensor_co2/fast", 2, &MyPlugin::callbackCo2Sensor, this);
+#endif
+
+  // start timer with a period of 100 ms
+  auto timer = new QTimer(this);
+  this->connect(timer, SIGNAL(timeout()), this, SLOT(process()));
+  timer->start(100);
 }
 
 void MyPlugin::stop(void)
 {
   ROS_FATAL("Emergency Stop! Kill drive node!");
-  QProcess::execute("rosnode", QStringList() << "kill" << "\francor_drivers_node");
+  QProcess::execute("rosnode", QStringList() << "kill" << "francor_drives_node");
 }
+
+void MyPlugin::process(void)
+{
+  ros::spinOnce();
+}
+
+#ifndef Q_MOC_RUN
+void MyPlugin::callbackCo2Sensor(const std_msgs::Int32& msg)
+{
+  std::cout << "co2 value = " << msg.data << std::endl;
+  _co2bar->setValue(msg.data);
+  widget_->update();
+}
+#endif
 
 } // end namespace 
 
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
+
+#ifndef Q_MOC_RUN
   ros::init(argc, argv, "gui_for_schmiddey");
+#endif
 
   my_namespace::MyPlugin plugin;
 
--- francor_rqt_plugins/src/my_plugin.h
+++ francor_rqt_plugins/src/my_plugin.h
@@ -4,7 +4,15 @@
 // #include <rqt_gui_cpp/plugin.h>
 // #include <ui_my_plugin.h>
 #include <QWidget>
- 
+
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+
+#include <std_msgs/Int32.h>
+#endif
+
+class QProgressBar;
+
 namespace my_namespace
 { 
 
@@ -18,10 +26,21 @@ public:
 
 public slots:
   void stop(void);
+  void process(void);
 
 private: 
+  void callbackCo2Sensor(const std_msgs::Int32& msg);
+
   // Ui::MyPluginWidget ui_; 
-  QWidget* widget_; 
+  QWidget* widget_;
+  QProgressBar* _co2bar;
+
+#ifndef Q_MOC_RUN
+  ros::NodeHandle _priv_nh;
+  ros::NodeHandle _nh;
+
+  ros::Subscriber _sub_co2;
+#endif
 };
 
 } // namespace
